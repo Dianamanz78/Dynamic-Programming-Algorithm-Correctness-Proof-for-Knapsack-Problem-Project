17bfaa745ada1fa4b947c8c9ad2dcac7bbede69b
Dianamanz78 <Dianamanz78@outlook.com>
2026-04-26 14:32:11 -0700

    Update greedy_approach.py

--- greedy_approach.py
+++ greedy_approach.py
@@ -1,1 +1,29 @@
+def greedyKnapsack(W, val, wt):
+    # get ratios
+    ratios = []
+    for x in range(len(val)):
+        ratios.append(val[x] / wt[x])
+    
+    # sort
+    sorted_indices = sorted(range(len(val)), key=lambda i: ratios[i], reverse=True)
 
+    currWt = 0
+    maxVal = 0
+    itemsPicked = []
+    
+    for i in sorted_indices:
+        if currWt + wt[i] <= W:
+            currWt = currWt + wt[i]
+            maxVal = maxVal + val[i]
+            itemsPicked.append(val[i])
+        else:
+            continue
+    
+    return maxVal, itemsPicked
+
+W = 4
+val = [1, 2, 3, 5]
+wt = [4, 5, 1, 2]
+maxVal, itemsPicked = greedyKnapsack(W, val, wt)
+print("max value: ", maxVal)
+print("items picked: ", itemsPicked)
