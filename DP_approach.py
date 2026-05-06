e479dc6260764c981d24b23c1b1daf3c1785bfa0
Dianamanz78 <Dianamanz78@outlook.com>
2026-05-05 23:46:42 -0700

    Update DP_approach.py

--- DP_approach.py
+++ DP_approach.py
@@ -39,12 +39,43 @@ def DP(W, val, wt):
     itemsPicked = getPickedItems(W, val, wt, table)
     return table[n][W], itemsPicked
     
-W = 7
-val = [16, 19, 23, 28]
-wt  = [2, 3, 4, 5]
-
-
+W = 3
+val = [5, 6, 7]
+wt  = [2, 1, 1]
 
 maxVal, itemsPicked = DP(W, val, wt)
 print("max value: ", maxVal)
 print("value of items picked: ", itemsPicked)
+
+W = 5
+val = [15, 16, 4]
+wt  = [4, 5, 6]
+
+maxVal, itemsPicked = DP(W, val, wt)
+print("max value: ", maxVal)
+print("value of items picked: ", itemsPicked)
+
+W = 100
+val = [25, 13, 13]
+wt  = [75, 50, 50]
+
+maxVal, itemsPicked = DP(W, val, wt)
+print("max value: ", maxVal)
+print("value of items picked: ", itemsPicked)
+
+W = 10
+val = [20, 15, 2]
+wt  = [10, 2, 4]
+
+maxVal, itemsPicked = DP(W, val, wt)
+print("max value: ", maxVal)
+print("value of items picked: ", itemsPicked)
+
+W = 7
+val = [16, 19, 23, 28]
+wt  = [2, 3, 4, 5]
+
+maxVal, itemsPicked = DP(W, val, wt)
+print("max value: ", maxVal)
+print("value of items picked: ", itemsPicked)
+
